db0b70fa3254b8d80b4ab42a7648e3d59ac67656
owen t <2owen.thomas@gmail.com>
2020-05-22 13:07:50 +0100

    Don't require port 443

--- checkConnection.sh
+++ checkConnection.sh
@@ -10,7 +10,7 @@ checkConnection() {
         #Inner control loop
         for SITE in $(cat $WEBSITES_FILE); do
             TIMESTAMP=$(date +%T-%d\-%m\-%y)
-            RESULT=$(curl --silent ${SITE}:443 -w %{time_connect} 2>&1 | tail -n 1)
+            RESULT=$(curl --silent ${SITE} -w %{time_connect} 2>&1 | tail -n 1)
             printf "%-18s %-30s %-15s\n" $TIMESTAMP $SITE $RESULT
         done
        sleep $FREQUENCY_SECONDS
