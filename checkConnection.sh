a04b241802ad399ae4084d3322e2121e42e3949f
owenthomas17 <2owen.thomas@gmail.com>
2020-12-02 19:58:35 +0000

    Update checkConnection.sh

--- checkConnection.sh
+++ checkConnection.sh
@@ -5,15 +5,15 @@ checkConnection() {
     local FREQUENCY_SECONDS="${2:-5}"
 
     # Outer control loop
-    printf "%-20s %-30s %-15s %-5s\n" Timestamp Site DNS_RTT Request_RTT 
+    printf "%-20s %-30s %-15s %-5s\n" Timestamp Site DNS_RTT TIME_CONNECT
     while true; do
         #Inner control loop
         for SITE in $(cat $WEBSITES_FILE); do
             TIMESTAMP=$(date +%T-%d\-%m\-%y)
             RESULT=$(curl --silent ${SITE} -w "%{time_namelookup}\n%{time_connect}\n" -o /dev/null)
             DNS_RTT=$(echo $RESULT | awk 'FNR==1')
-            REQUEST_RTT=$(echo $RESULT | awk 'FNR==2')
-            printf "%-20s %-30s %-15s %-5s\n" $TIMESTAMP $SITE $DNS_RTT $REQUEST_RTT
+            TIME_CONNECT=$(echo $RESULT | awk 'FNR==2')
+            printf "%-20s %-30s %-15s %-5s\n" $TIMESTAMP $SITE $DNS_RTT $TIME_CONNECT
         done
        sleep $FREQUENCY_SECONDS
     done
