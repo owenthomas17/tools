7185ea4be1463de87c1fef31f65571549d5fab29
owen t <2owen.thomas@gmail.com>
2021-09-05 12:15:32 +0100

    generate-certs: various updates
    Rename CERT_TYPE variable to COMMON_NAME. Change default cert file directory. Parameterise some of the config files and set defaults.

--- createPrivateCA/generate-certs.sh
+++ createPrivateCA/generate-certs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+DOMAIN=example.net
+DEFAULT_BIT_LENGTH="4096"
+
 function createCertConfig {
-	local CERT_TYPE=$1
-	cat > "${CERT_TYPE}".cnf << EOF
+	local COMMON_NAME=$1
+	cat > public/"${COMMON_NAME}".cnf << EOF
 [ req ]
-default_bits = 4096
+default_bits = "${DEFAULT_BIT_LENGTH}"
 prompt = no
 default_md = sha256
 req_extensions = req_ext
@@ -15,43 +18,43 @@ C = GB
 ST = Sussex
 L = London
 O = thommo
-OU = "${CERT_TYPE}"
-CN = "${CERT_TYPE}.example.net"
+OU = "${COMMON_NAME}"
+CN = "${COMMON_NAME}.${DOMAIN}"
 
 [ req_ext ]
 subjectAltName = @alt_names
 
 [ alt_names ]
-DNS.1 = "${CERT_TYPE}"
-DNS.2 = "${CERT_TYPE}.example.net"
+DNS.1 = "${COMMON_NAME}"
+DNS.2 = "${COMMON_NAME}.${DOMAIN}"
 EOF
 }
 
 function createCSR {
   local CSR_TYPE=$1
-	openssl genrsa -out "${CSR_TYPE}".key 2048
-	openssl req -new -key "${CSR_TYPE}".key -out "${CSR_TYPE}".csr -config "${CSR_TYPE}".cnf
+	openssl genrsa -out public/"${CSR_TYPE}".key "${DEFAULT_BIT_LENGTH}"
+	openssl req -new -key public/"${CSR_TYPE}".key -out public/"${CSR_TYPE}".csr -config public/"${CSR_TYPE}".cnf
 }
 
 function createCert {
-	local CERT_TYPE=$1
-	createCertConfig "${CERT_TYPE}"
-	createCSR "${CERT_TYPE}"
+	local COMMON_NAME=$1
+	createCertConfig "${COMMON_NAME}"
+	createCSR "${COMMON_NAME}"
 }
 
 function signCert {
-	local CERT_TYPE=$1
-	openssl x509 -req -in "${CERT_TYPE}".csr -CA rootCA.crt -CAkey rootCA.key \
-	-CAcreateserial -out "${CERT_TYPE}".crt -days 10000 \
-	-extfile "${CERT_TYPE}".cnf
+	local COMMON_NAME=$1
+	openssl x509 -req -in public/"${COMMON_NAME}".csr -CA ca/rootCA.crt -CAkey ca/rootCA.key \
+	-CAcreateserial -out public/"${COMMON_NAME}".crt -days 10000 \
+	-extfile public/"${COMMON_NAME}".cnf
 }
 
 function main {
-	mkdir -p certs
+	mkdir -p certs/public
 	cd certs || exit 1
-	for cert in client server; do
-		createCert $cert
-		signCert $cert
+	for CN in client server; do
+		createCert $CN
+		signCert $CN
 	done
 }
 
